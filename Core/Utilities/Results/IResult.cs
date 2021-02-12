ff787df3512a3189cf2cbc533c90df791fa1f4a0
kgncsr <69785776+kgncsr@users.noreply.github.com>
2021-02-12 16:30:37 +0300

    12.02.2021

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IResult
+    {//void operasyonlarım icin
+        bool Success { get; }
+        string Message { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public bool Success { get; } //sadece okunabilir set özelliğini ctor
+
+        public string Message { get; }
+    }
+}
